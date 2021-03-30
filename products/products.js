f686d5d80de2ae45751eba90d85605319db9c599
Katherine Tam <katherinemtam@gmail.com>
2021-03-30 13:57:41 -0700

    add product page, add products.js array

---
+++ products/products.js
@@ -0,0 +1,42 @@
+export const sanX = [
+    {
+        name: 'Shirokuma',
+        type: 'polar bear',
+        img: 'http://www.san-x.jp/img/characters/sumikko/shirokuma.png',
+        description: 'Shirokuma is a shy bear who migrated south to avoid the cold. Shirokuma feels most calm while drinking hot tea in a corner.',
+        isAnimal: true,
+        price: 2,
+    },
+    {
+        name: 'Penguin?',
+        type: 'penguin?',
+        img: 'http://www.san-x.jp/img/characters/sumikko/shirokuma.png',
+        description: 'Am I even a penguin? Penguin has no confidence. In the past, there might have been a plate on Penguin’s head...',
+        isAnimal: true,
+        price: 2,
+    },
+    {
+        name: 'Tonkatsu',
+        type: 'pork cutlet',
+        img: 'http://www.san-x.jp/img/characters/sumikko/tonkatu.png',
+        description: 'The end of a tonkatsu. Made of 1% meat, 99% fat. Left behind for being too oily...',
+        isAnimal: false,
+        price: 2,
+    },
+    {
+        name: 'Neko',
+        type: 'cat',
+        img: 'http://www.san-x.jp/img/characters/sumikko/neko.png',
+        description: 'Shy and timid, Neko usually hides in the corner. Neko worries about its body shape.',
+        isAnimal: true,
+        price: 2,
+    },
+    {
+        name: 'Tokage',
+        type: 'lizard',
+        img: 'http://www.san-x.jp/img/characters/sumikko/tokage.png',
+        description: 'Is actually the last of the dinosaurs. It pretends to be a lizard to avoid being caught. Slowly warming up to Nisetsumuri.',
+        isAnimal: true,
+        price: 2,
+    }
+];
