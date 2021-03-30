0b166426231bc69f517c35a0ceeaa977dcd2013a
Katherine Tam <katherinemtam@gmail.com>
2021-03-30 15:57:44 -0700

    TDD DOM render function, generate product list

---
+++ products/product.js
@@ -0,0 +1,9 @@
+import { sanX } from '../data.js';
+import { sanXProfileLi } from '../utils.js';
+
+const ul = document.querySelector('.char-list');
+
+for (let sanXChar of sanX) {
+    const li = sanXProfileLi(sanXChar);
+    ul.append(li);
+}
