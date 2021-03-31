5131ca181766e13c2f727fdb8b57a19dc1c2c44b
Katherine Tam <katherinemtam@gmail.com>
2021-03-31 13:16:28 -0700

    add cart data

---
+++ cart/cart-data.js
@@ -0,0 +1,14 @@
+export const cart = [
+    {
+        id: 'one',
+        quantity: 2
+    },
+    {
+        id: 'three',
+        quantity: 1
+    },
+    {
+        id: 'five',
+        quantity: 4
+    }
+];
--- products/products.js
+++ products/products.js
@@ -1,4 +1,4 @@
-import { sanX } from '../products-data.js';
+import { sanX } from './products-data.js';
 import { sanXProfileLi } from '../utils.js';
 
 const ul = document.querySelector('.char-list');
